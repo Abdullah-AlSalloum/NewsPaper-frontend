c7690e3c6ed20171c4ca77d94dd533e14aca2543
Abdullah AL Salloum <abdullahalsalloum06@gmail.com>
2025-02-16 19:10:15 +0300

    Update deploy configuration for GitHub Pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+  import { HashRouter } from "react-router-dom";
 import App from './App.jsx'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+   <HashRouter>
     <App />
   </HashRouter>
 );
